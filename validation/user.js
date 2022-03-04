432c10d084ae603fc6e0c4cda8e312e8f323182d
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-04 16:57:06 +0100

    Removed "update user validation rules"

--- validation/user.js
+++ validation/user.js
@@ -4,31 +4,33 @@ const models = require('../models');
 //* Create User validation rules
 
  const createRules = [
-	// Check if email alredy exists
-	body('email').exists().isLength({ min: 3 }).custom(async value => {
-		const email = await new models.User({ email: value }).fetch({ require: false });
-		if (email) {
-			return Promise.reject("Email already exists.");
-		}
-	
-		return Promise.resolve();
-	}),
+
 	body('email').exists().isLength({ min: 4 }),
 	body('password').exists().isLength({ min: 6 }),
 	body('first_name').exists().isLength({ min: 3 }),
 	body('last_name').exists().isLength({ min: 3 }),
+
+	// Check if email alredy exists
+	body('email').exists().isLength({ min: 3 }).custom(async value => {
+		const email = await new models.User({ email: value }).fetch({ require: false });// require: false = OK to not found email. Otherwise CRASH BAM BOOM!!!
+		if (email) {
+			return Promise.reject("Email already exists.");
+		}
+
+		return Promise.resolve();
+	}),
  ];
  
-//* Update User Validation Rules
+//! //* Update User Validation Rules
 
- const updateRules = [
-	body('password').optional().isLength({ min: 4 }),
-	body('first_name').optional().isLength({ min: 2 }),
-	body('last_name').optional().isLength({ min: 2 }),
-];
+//  const updateRules = [
+// 	body('password').optional().isLength({ min: 4 }),
+// 	body('first_name').optional().isLength({ min: 2 }),
+// 	body('last_name').optional().isLength({ min: 2 }),
+// ];
 
  module.exports = {
 	 createRules,
-	 updateRules,
+	//! updateRules,
  }
  
