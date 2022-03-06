4493c1600ce33732a259cc09ac34b0b2e809c469
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-06 19:08:48 +0100

    cleaned up code

--- controllers/register_controller.js
+++ controllers/register_controller.js
@@ -1,62 +1,58 @@
-/**
- * Register Controller
- */
 
- const bcrypt = require('bcrypt');
- const debug = require('debug')('photo_app:register_controller');
- const { matchedData, validationResult } = require('express-validator');
- const models = require('../models');
+//! RÖR EJ
+
+// REGISTER CONTROLLER
+
+const bcrypt = require('bcrypt');
+const debug = require('debug')('photo_app:register_controller');
+const { matchedData, validationResult } = require('express-validator');
+const models = require('../models');
  
- /**
-  * Register a new user
-  *
-  * POST /
-  */
- const register = async (req, res) => {
-	 // check for any validation errors
-	 const errors = validationResult(req);
-	 if (!errors.isEmpty()) {
-		 return res.status(422).send({ status: 'fail', data: errors.array() });
-	 }
+
+//* POST- Register a new user 
  
-	 // get only the validated data from the request
-	 const validData = matchedData(req);
+const register = async (req, res) => { 
+	// check for any validation errors
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).send({ status: 'fail', data: errors.array() });
+	};
  
-	 console.log("The validated data:", validData);
+	// Get ONLY the validated data from the request
+	const validData = matchedData(req);
+  
+	// Generate a hash password of validData.password and overwrite the existing validData.password
+	try {
+		validData.password = await bcrypt.hash(validData.password, 10);
  
-	 // generate a hash of `validData.password`
-	 // and overwrite `validData.password` with the generated hash
-	 try {
-		 validData.password = await bcrypt.hash(validData.password, 10);
+	} catch (error) {
+		res.status(500).send({
+			status: 'error',
+		 	message: 'Exception thrown when hashing the password.',
+		});
+		throw error;
+	}
  
-	 } catch (error) {
-		 res.status(500).send({
-			 status: 'error',
-			 message: 'Exception thrown when hashing the password.',
-		 });
-		 throw error;
-	 }
+	try {
+		const user = await new models.User(validData).save();
+		debug("Created new user successfully: %O", user);
  
-	 try {
-		 const user = await new models.User(validData).save();
-		 debug("Created new user successfully: %O", user);
+		res.send({
+			status: 'success',
+			data: {
+				user,
+			},
+		})
  
-		 res.send({
-			 status: 'success',
-			 data: {
-				 user,
-			 },
-		 });
+	} 	catch (error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown in database when creating a new user.',
+		});
+		throw error;
+	};
+};
  
-	 } catch (error) {
-		 res.status(500).send({
-			 status: 'error',
-			 message: 'Exception thrown in database when creating a new user.',
-		 });
-		 throw error;
-	 }
- }
- 
- module.exports = {
-	 register,
- }
+module.exports = {
+	register,
+};
