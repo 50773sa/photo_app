4de5515c8451c494f4990fa899b9c3be7bae025d
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-06 19:10:49 +0100

    Removed unnecessary GETs and PUTs

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/register_controller');
-//const userController = require('../controllers/user_controller');
-const userValidationRules = require('../validation/user');
-
-// //! Get all users
-// router.get('/', userController.index);
-
-// //! Get a single user
-// router.get('/:userId', userController.show);
-
-// //! Update a specific user
-// router.put('/:userId', userValidationRules.updateRules, userController.update);
+const profileValidationRules = require('../validation/user');
 
 //* Register a new user
-router.post('/register', userValidationRules.createRules, registerController.register);
+router.post('/register', profileValidationRules.createRules, registerController.register);
 
 
 module.exports = router;
+
+
+//!Rör ej
 
 
 
