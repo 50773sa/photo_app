7c23887159008ba608f8442a9172450f5f5eaeac
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-07 09:56:50 +0100

    Reject POST if album already exists

--- validation/album.js
+++ validation/album.js
@@ -1,27 +1,37 @@
-/**
- * Album Validation Rules
- */
 
- const { body } = require('express-validator');
+ //Album Validation Rules
+
+
+//! OK för albums. 
+
+
+const { body } = require('express-validator');
+const models = require('../models');
  
  
  //*  Create Album validation rules
 
- const createRules = [
-     body('title').exists().isLength({ min: 3 }),
-     body('user_id').exists().isInt({ min: 1 }),
- ];
-
- //* Update Album validation rules
-
- const updateRules = [
-    body('title').optional().isLength({ min: 3 }),
+const createRules = [
+    body('title').exists().isLength({ min: 3 }).custom(async value => {
+		const title = await new models.Album({ title: value }).fetch({ require: false });
+		if (title) {
+			return Promise.reject("Title already exists.");
+		}
+	
+		return Promise.resolve();
+	}),
 ];
 
 
+//* Update Album validation rules
+
+const updateRules = [
+    body('title').optional().isLength({ min: 3 }),
+];
+
  
- module.exports = {
-     createRules,
-     updateRules
- }
+module.exports = {
+    createRules,
+    updateRules,
+};
  
