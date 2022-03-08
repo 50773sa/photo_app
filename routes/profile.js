f1ffaef7eb2a3fcb43def191ef137318798d5209
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-08 18:42:46 +0100

    Removed all methods for authenticated users, exept from profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,19 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profile_controller');
-const albumController = require('../controllers/album_controller');
-const albumValidationRules = require('../validation/album');
+
 
 //* Get the authenticated user's profile
 router.get('/', profileController.getProfile);
 
-
-//*  Get authenticated user's albums
-router.get('/albums', profileController.getAlbums);
-
-router.post('/albums', albumValidationRules.createRules, profileController.addAlbum);
-
-//* Get authenticated user's photos
-router.get('/photos', profileController.getPhotos);
 
 module.exports = router;
