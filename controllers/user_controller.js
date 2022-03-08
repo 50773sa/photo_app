6620eedc541d7d72ce7814b20591fdbda2e8c21c
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-08 19:12:11 +0100

    Former profile_controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,50 +1,97 @@
-// /**
-//  * User Controller
-//  */
 
-//  const debug = require('debug')('photo_app:user_controller');
-//  const { matchedData, validationResult } = require('express-validator');
-//  const models = require('../models');
+/* USER_CONTROLLER */
+
+
+const bcrypt = require('bcrypt');
+const debug = require('debug')('photo_app:profile_controller');
+const { matchedData, validationResult } = require('express-validator');
+const models = require('../models');
+
  
 
-//  //------------------------------------------------------------------
-//  /**
-//   * Store a new resource
-//   *
-//   * POST /
-//   */
-//  const store = async (req, res) => {
-// 	 // check for any validation errors
-// 	 const errors = validationResult(req);
-// 	 if (!errors.isEmpty()) {
-// 		 return res.status(422).send({ status: 'fail', data: errors.array() });
-// 	 }
-//  	//----------------
+//*  GET authenticated user's profile
 
-// 	 // get only the validated data from the request
-// 	 const validData = matchedData(req);
- 
-// 	 try {
-// 		 const user = await new models.User(validData).save();
-// 		 debug("Created new user successfully: %O", user);
- 
-// 		 res.send({
-// 			 status: 'success',
-// 			 data: user,
-// 		 });
- 
-// 	 } catch (error) {
-// 		 res.status(500).send({
-// 			 status: 'error',
-// 			 message: 'Exception thrown in database when creating a new user.',
-// 		 });
-// 		 throw error;
-// 	 }	
-//  }
-
-// //------------------------------------------------------------------
-//  module.exports = {
+const getUser = async (req, res) => {
 	
-// 	 store,
-//  }
+	res.send({
+		status: 'success',
+		data: {
+			user: req.user,	 
+		}
+	});
+};
  
+
+//*  GET authenticated user's albums
+
+const getAlbums = async (req, res) => {
+	
+	// "Called lazy load" = fetch the the albums-relation
+	await req.user.load('albums');
+ 
+	res.status(200).send({
+		status: 'success',
+		data: {
+			albums: req.user.related('albums'),
+		},
+	});
+};
+
+
+//*  POST authenticated user's albums
+
+const addAlbum = async (req, res) => {
+
+	// check for any validation errors
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(422).send({ status: 'fail', data: errors.array() });
+	}
+
+	// get only the validated data from the request
+	const validData = matchedData(req);
+	validData.user_id = req.user.get('id')
+
+	
+	try {
+		const album = await new models.Album(validData).save();
+		debug("Added album to user successfully: %O", album);
+
+		res.send({
+			status: 'success',
+			data: album
+		});
+
+	} catch (error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown in database when adding a album to a user.',
+		});
+		throw error;
+	}
+}
+
+
+ //* GET authenticated user's photos
+
+const getPhotos = async (req, res) => {
+
+	// Load photo relationship
+	await req.user.load('photos');
+
+	res.status(200).send({
+		status: 'success',
+		data: {
+			photos: req.user.related('photos'),
+		},
+	});
+};
+ 
+module.exports = {
+
+	getUser,
+	getAlbums,
+	addAlbum,
+	getPhotos,
+};
