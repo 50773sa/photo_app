079592520b6c6dd4ecc26407835415eaa5595f52
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-08 19:13:40 +0100

    Added user-registration rules

--- validation/user.js
+++ validation/user.js
@@ -1,10 +1,38 @@
 
+
+// REGISTER USER
 // USER_VALIDATION RULES
+
+//--------------------------
 
  const { body } = require('express-validator');
  const models = require('../models');
  
  
+ //  REGISTRATION VALIDATION RULES
+
+ const { body } = require('express-validator');
+ const models = require('../models');
+
+
+ //* Create User Registration rules
+
+ const createRegistrationRules = [
+	// Check if email alredy exists
+	body('email').exists().isEmail().trim().isLength({ min: 8 }).custom(async value => {
+		const email = await new models.User({ email: value }).fetch({ require: false });
+		if (email) {
+			return Promise.reject("Email already exists.");
+		}
+	
+		return Promise.resolve();
+	}),
+	body('password').exists().isLength({ min: 6 }),
+	body('first_name').exists().isLength({ min: 3 }),
+	body('last_name').exists().isLength({ min: 3 }),
+ ];
+
+
  //* Create User validation rules
  
  const createRules = [
@@ -25,5 +53,6 @@
 ];
 
  module.exports = {
+	createRegistrationRules,
 	 createRules,	 
  };
