4fec2dfcdfdedeecffeaef96d9493185e09e1625
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-10 17:36:12 +0100

    Final commit

--- controllers/register_controller.js
+++ controllers/register_controller.js
@@ -10,7 +10,8 @@ const models = require('../models');
 //* POST- Register a new user 
  
 const register = async (req, res) => { 
-	// check for any validation errors
+
+	// Validation errors?
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(422).send({ status: 'fail', data: errors.array() });
@@ -18,18 +19,21 @@ const register = async (req, res) => {
  
 	// Get ONLY the validated data from the request
 	const validData = matchedData(req);
+
   
-	// Generate a hash password of validData.password and overwrite the existing validData.password
+	// Generate a hash password of validData.password 
+	// and overwrite the existing validData.password
 	try {
 		validData.password = await bcrypt.hash(validData.password, 10);
  
-	} catch (error) {
+	} 	catch (error) {
 		res.status(500).send({
 			status: 'error',
 		 	message: 'Exception thrown when hashing the password.',
 		});
+
 		throw error;
-	}
+	};
  
 	try {
 		const user = await new models.User(validData).save();
@@ -47,6 +51,7 @@ const register = async (req, res) => {
 			status: 'error',
 			message: 'Exception thrown in database when creating a new user.',
 		});
+
 		throw error;
 	};
 };
